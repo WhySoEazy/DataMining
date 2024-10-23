ab340705157935ffe11d86af1f012f28ba610043
Nguyễn Triết <trietnguyen23012003@gmail.com>
2024-10-23 22:36:46 +0700

    update

--- LAB_2_Dashboard_and_Model/main.py
+++ LAB_2_Dashboard_and_Model/main.py
@@ -6,8 +6,6 @@ import matplotlib.pyplot as plt
 import pickle
 import sklearn
 
-st.page_link("pages/main.py", label="Home", icon="🏠")
 st.page_link("pages/data.py", label="Data", icon="📚")
 st.page_link("pages/dashboard.py", label="Dashboard", icon="1️⃣")
 st.page_link("pages/model.py", label="Model", icon="2️⃣")
-st.page_link("http://www.google.com", label="Google", icon="🌎")
